e0f1691d25da1517c5d9243bceb22d7e292ee0fa
김혜린 <mye785c83@gmail.com>
2022-08-25 13:38:23 +0900

    :zap: 기본 설정 완료
    - mongo db 연결
    - Module 정리
    - LoggerMiddleware 연결(dev mode에서만)

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,18 +1,37 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { LoggerMiddleware } from "./common/middleware/logger.middleware";
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ScoreModule } from './score/score.module';
 import { ConsultingModule } from './consulting/consulting.module';
-import { CheckboxformModule } from './checkboxform/checkboxform.module';
-import { ConsultingFormModule } from './consulting-form/consulting-form.module';
 import { CheckboxFormModule } from './checkbox-form/checkbox-form.module';
 import { ConsultingRequestModule } from './consulting-request/consulting-request.module';
+import { ConfigModule } from '@nestjs/config';
+import mongoose from 'mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/tmp'), UserModule, ScoreModule, ConsultingModule, CheckboxformModule, ConsultingFormModule, CheckboxFormModule, ConsultingRequestModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }), 
+    UserModule,
+    ScoreModule,
+    ConsultingModule, 
+    CheckboxFormModule,
+    ConsultingRequestModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  private readonly isDev: boolean = process.env.MODE === 'dev' ? true : false;
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+    mongoose.set('debug', this.isDev);
+  }
+}
