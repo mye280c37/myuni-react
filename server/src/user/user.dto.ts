04f0ac202f4e89ef8a2cd26f3fe35c774567dac8
KimHyerin <mye785c83@gmail.com>
2022-09-17 20:31:18 +0900

    :memo: swagger example 추가

--- server/src/user/user.dto.ts
+++ server/src/user/user.dto.ts
@@ -1,14 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNumber, IsString } from "class-validator";
 
 export class UserDto {
     @IsString()
+    @ApiProperty({example: '아무개'})
     name: String;
     @IsString()
+    @ApiProperty({example: 'm'})
     sex: String;
     @IsNumber()
+    @ApiProperty({example: '19'})
     age: Number;
     @IsString()
+    @ApiProperty({example: '01011112222'})
     phone: String;
     @IsString()
+    @ApiProperty({example: '1002-987-123456 00은행 아무개'})
     refundAccount: String;
 }
