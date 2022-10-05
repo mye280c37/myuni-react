3f46aa7db0bdc5f1a206d21388f0e34fb44ffe3c
KimHyerin <mye785c83@gmail.com>
2022-10-05 11:16:16 +0900

    :bug: CORS 허용

--- server/src/main.ts
+++ server/src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
   }
-  
-  await app.listen(8000);
+  app.enableCors();
+  await app.listen(process.env.PORT);
 }
 bootstrap();
