cd79394aac7bf7836a75bdbe57440cf23232cca4
김혜린 <mye785c83@gmail.com>
2022-11-14 11:57:20 +0900

    :zap: state 반영, 각 요소 반영 방식 변경

--- src/components/forms/CheckBoxFormTemplate.js
+++ src/components/forms/CheckBoxFormTemplate.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {useEffect} from "react";
 import TextField from '@mui/material/TextField';
-import { FormControl } from '@mui/material';
+import { FormControl, Typography } from '@mui/material';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import FormGroup from '@mui/material/FormGroup';
@@ -11,15 +11,9 @@ export default function CheckBoxFormTemplate({ name, etcIdx=-1, values, onParent
   const {form, onCheckedChange, onEtcChange} = useCheckBoxForm(values);
   const {checked, labels, etc} = form;
 
-  const checkedHandler = (e) => {
-    onCheckedChange(e);
+  useEffect(()=>{
     onParentChange(name, form);
-  }
-
-  const etcHandler = (e) => {
-    onEtcChange(e);
-    onParentChange(name, form);
-  }
+  }, [checked, etc, form, name, onParentChange]); 
   
   const showAdditionalTextField = () => {
       if(etcIdx!==-1 && checked[etcIdx]){
@@ -29,52 +23,33 @@ export default function CheckBoxFormTemplate({ name, etcIdx=-1, values, onParent
               id="etc"
               name="etc"
               value={etc}
-              onChange={etcHandler}
+              onChange={onEtcChange}
               label="기타"
               fullWidth
               />
           );
       }
   };
-
-  const showEntireCheckBox= () =>{
-    const checkboxList=[];
-
-    labels.map((value, idx)=>{
-      if(etcIdx !== idx){
-        checkboxList.push(
-          <FormControlLabel
-            control={
-              <Checkbox name={idx} checked={checked[idx]} onChange={checkedHandler}/>
-            }
-            label={value}
-          />
-          
-        );
-      }
-      else{
-        checkboxList.push(
-          <React.Fragment>
-            <FormControlLabel
-              control={
-                <Checkbox name={idx} checked={checked[idx]} onChange={checkedHandler}/>
-              }
-              label={value}
-            />
-            {showAdditionalTextField()}
-          </React.Fragment>
-        );
-      }
-    })
-
-    return checkboxList;
-  };
   
   return (
     <React.Fragment>
         <FormControl required variant="standard" fullWidth>
           <FormGroup>
-              {showEntireCheckBox()}
+              {
+                labels.map((value, idx) => {
+                  return (
+                    <React.Fragment>
+                      <FormControlLabel
+                        control={
+                          <Checkbox name={idx} checked={checked[idx]} onChange={onCheckedChange}/>
+                        }
+                        label={value}
+                      />
+                      {idx===etcIdx?showAdditionalTextField():<Typography></Typography>}
+                    </React.Fragment>
+                  );
+                })
+              }
           </FormGroup>
         </FormControl>
       </React.Fragment>
