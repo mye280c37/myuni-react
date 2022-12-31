db8a4652191263c78a1593f906cc2ee6c270497b
김혜린 <mye785c83@gmail.com>
2022-12-31 11:25:25 +0900

    :sparkles: main 페이지 완성

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,58 @@
-import * as React from 'react';
-import PageLayout from './PageLayout';
+import React, {useEffect, useState, useRef} from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Header from '../components/common/Header';
+import Footer from '../components/common/Footer';
+
 
 export default function Main() {
+    const [count, setCount] = useState(0);
+    const savedCallback = useRef();
+
+    const imageList = [
+        'img/main/대학입시전략및실전설명회.jpeg',
+        'img/main/면접컬설팅.jpeg',
+        'img/main/global_hr_forum.jpeg'
+    ];
+
+    const imageStyle = {
+        'width': '100%',
+        'height': '70vh',
+        'filter': 'brightness(60%)',
+        'object-fit': 'cover'
+    }
+
+    const callback = () => {
+        if(count === imageList.length - 1){
+            setCount(0);
+        }
+        else setCount(count + 1);
+    }
+
+    useEffect(() => {
+        savedCallback.current = callback;
+    });
+
+    useEffect(() => {
+        const tick = () => {
+            savedCallback.current();
+        }
+
+        const timer = setInterval(tick, 3000);
+        return () => clearInterval(timer);
+    }, []);
 
     return (
-        <PageLayout title={"MYUNI"} secondary={"검정고시 출신자 맞춤 입시 정보 제공 홈페이지"}>
-            
-        </PageLayout>
+        <Container component="main" maxWidth="lg" sx={{ pt: { xs: 12, sm:15}, pb: 4, pl: { xs: 3, md: 3 }, pr: { xs: 3, md: 3 }, height: '100vh'}}>
+            <Header/>
+            {/* {count} */}
+            <Box sx={{ pl: {sm: 10, md: 1}, pr: {sm: 10, md: 1}, pb : {sm: 5, md: 1} }}>
+                <img style={imageStyle} alt="main-image" src={imageList[count]} />
+            </Box>
+            <Typography variant='h3' component='h1' sx={{ textAlign: 'right'}}>MYUNI</Typography>
+            <Typography variant='body2' component='h1' color="text.secondary" sx={{ mb: 8, textAlign: 'right' }}>검정고시 출신자 맞춤 입시 정보 제공 홈페이지</Typography>
+            <Footer/>
+        </Container>
     );
 }
