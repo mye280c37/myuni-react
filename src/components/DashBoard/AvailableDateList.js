914ea59a860e4ea96888939ec9ce51e54f4f0483
김혜린 <mye785c83@gmail.com>
2023-03-21 11:30:50 +0900

    :card_file_box: 컨설팅 신청 날짜 스키마 변경 반영

--- src/components/DashBoard/AvailableDateList.js
+++ src/components/DashBoard/AvailableDateList.js
@@ -18,9 +18,7 @@ import AddCircleIcon from '@mui/icons-material/AddCircle';
 const url = process.env.REACT_APP_API_URL;
 
 const columns = [
-    { id: 'date', label: '날짜' },
-    { id: 'timeFrom', label: '시작 시간'},
-    { id: 'timeTo', label: '종료 시간'}
+    { id: 'date', label: '날짜' }
 ];
 
 const AvailableDateField = () => {
@@ -30,28 +28,6 @@ const AvailableDateField = () => {
                 <TextField
                     id={'date'}
                     name={'date'}
-                    // value={form.value}
-                    // onChange={additionalFormItemHandler}
-                    fullWidth
-                    variant="outlined"
-                    size="small"
-                />
-            </TableCell>
-            <TableCell>
-                <TextField
-                    id={'timeFrom'}
-                    name={'timeFrom'}
-                    // value={form.value}
-                    // onChange={additionalFormItemHandler}
-                    fullWidth
-                    variant="outlined"
-                    size="small"
-                />
-            </TableCell>
-            <TableCell>
-                <TextField
-                    id={'timeTo'}
-                    name={'timeTo'}
                     // value={form.value}
                     // onChange={additionalFormItemHandler}
                     fullWidth
