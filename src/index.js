4286d63f93669901503dc7e0418e555fa8dd0b62
김혜린 <mye785c83@gmail.com>
2023-05-15 19:18:06 +0900

    :sparkles: cors 설정

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { CookiesProvider } from 'react-cookie';
+import axios from 'axios';
+
+axios.defaults.withCredentials = true;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <CookiesProvider>
-      <App />
-    </CookiesProvider>
-  </React.StrictMode>,
+  <CookiesProvider>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+  </CookiesProvider>,
   document.getElementById('root')
 );
 
