5e073f4afd9418cb2e19148ea53d28e2a1fff435
김혜린 <mye785c83@gmail.com>
2023-05-25 12:59:45 +0900

    :lipstick: 어드민 보드 드로어 구조 변경

--- src/components/DashBoard/DrawerContents.js
+++ src/components/DashBoard/DrawerContents.js
@@ -9,10 +9,10 @@ import DashboardIcon from '@mui/icons-material/Dashboard';
 import PeopleIcon from '@mui/icons-material/People';
 import BarChartIcon from '@mui/icons-material/BarChart';
 
-import AccordionDetails from '@mui/material/AccordionDetails';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MuiAccordion from '@mui/material/Accordion';
-import MuiAccordionSummary from '@mui/material/AccordionSummary';
+// import AccordionDetails from '@mui/material/AccordionDetails';
+// import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+// import MuiAccordion from '@mui/material/Accordion';
+// import MuiAccordionSummary from '@mui/material/AccordionSummary';
 
 const drawerWidth = 240;
 
@@ -22,43 +22,51 @@ const ListItemIcon = styled((props) => (
     minWidth: 44
   }));
 
-const Accordion = styled((props) => (
-    <MuiAccordion disableGutters elevation={0} square {...props} />
-  ))(({ theme }) => ({
-    border: 0,
-    // border: `1px solid ${theme.palette.divider}`,
-    // '&:not(:last-child)': {
-    //   borderBottom: 0,
-    // },
-    '&:before': {
-      display: 'none',
-    },
-  }));
+// const Accordion = styled((props) => (
+//     <MuiAccordion disableGutters elevation={0} square {...props} />
+//   ))(({ theme }) => ({
+//     border: 0,
+//     // border: `1px solid ${theme.palette.divider}`,
+//     // '&:not(:last-child)': {
+//     //   borderBottom: 0,
+//     // },
+//     '&:before': {
+//       display: 'none',
+//     },
+//   }));
 
-const AccordionSummary = styled((props) => (
-    <MuiAccordionSummary
-        expandIcon={<ExpandMoreIcon sx={{ mr: '1px' }} />}
-      {...props}
-    />
-  ))(({ theme }) => ({
-    '& .MuiAccordionSummary-content': {
-      marginTop: theme.spacing(0.2),
-      marginBottom: theme.spacing(0.2),
-    },
-  }));
+// const AccordionSummary = styled((props) => (
+//     <MuiAccordionSummary
+//         expandIcon={<ExpandMoreIcon sx={{ mr: '1px' }} />}
+//       {...props}
+//     />
+//   ))(({ theme }) => ({
+//     '& .MuiAccordionSummary-content': {
+//       marginTop: theme.spacing(0.2),
+//       marginBottom: theme.spacing(0.2),
+//     },
+//   }));
 
 function DrawerContents(props) {
   return (
     <React.Fragment>
-      <ListItemButton sx={{ pt:0, pb:0, minHeight: 48 }}>
+      <ListItemButton sx={{ pt:0, pb:0, minHeight: 48 }} onClick={()=>props.onClick(-1)}>
+        <ListItemIcon>
+          <BarChartIcon />
+        </ListItemIcon>
+        <Link href='#' color="inherit" underline="none">
+          <ListItemText primary="컨설팅 신청 관리" />
+        </Link>
+      </ListItemButton>
+      <ListItemButton sx={{ pt:0, pb:0, minHeight: 48 }} onClick={()=>props.onClick(0)}>
         <ListItemIcon>
           <DashboardIcon />
         </ListItemIcon>
         <Link href='#' color="inherit" underline="none">
-          <ListItemText primary="Dashboard" />
+          <ListItemText primary="컨설팅 신청 폼 관리" />
         </Link>
       </ListItemButton>
-      <ListItemButton sx={{ p:0 }}>
+      {/* <ListItemButton sx={{ p:0 }}>
       <Accordion sx={{ width: drawerWidth }}>
               <AccordionSummary
               expandIcon={<ExpandMoreIcon />}
@@ -93,8 +101,8 @@ function DrawerContents(props) {
                   </ListItemButton>
               </AccordionDetails>
           </Accordion>
-      </ListItemButton>
-      <ListItemButton sx={{ pt:0, pb:0, minHeight: 48 }} onClick={()=>props.onClick(2)}>
+      </ListItemButton> */}
+      <ListItemButton sx={{ pt:0, pb:0, minHeight: 48 }} onClick={()=>props.onClick(0)}>
         <ListItemIcon>
           <PeopleIcon />
         </ListItemIcon>
